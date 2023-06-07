f6658d093257a70b5ec6b118ee70e845c0c29415
Muaz Abdin <muathabd25@gmail.com>
2023-06-07 15:58:30 -0400

    set init state & create the context

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -1,1 +1,17 @@
 import React, { createContext, useReducer } from 'react';
+
+// 1. Sets the initial state when the app loads
+const initialState = {
+    expenses: [
+        { id: "Shirt", name: 'Shirt', quantity: 0, unitprice: 500 },
+        { id: "Jeans", name: 'Jeans', quantity: 0, unitprice: 300 },
+        { id: "Dress", name: 'Dress', quantity: 0, unitprice: 400 },
+        { id: "Dinner set", name: 'Dinner set', quantity: 0, unitprice: 600 },
+        { id: "Bags", name: 'Bags', quantity: 0, unitprice: 200 },
+    ],
+    Location: '£'
+};
+
+// 2. Creates the context this is the thing our components import and use to get the state
+export const AppContext = createContext();
+
